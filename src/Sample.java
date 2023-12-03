b1018a37c5dc6366234588c8f4d2807dcc0e71d5
MuaajSombare <sombaremuaaj6@gmail.com>
2023-12-03 13:25:58 +0530

    adding file- making connection with database

--- src/Sample.java
+++ src/Sample.java
@@ -4,14 +4,14 @@ public class Sample {
     private static final String username = "root";
     private static final String password = "1234";
     public static void main(String[] args) throws ClassNotFoundException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
+        Class.forName("com.mysql.cj.jdbc.Driver");//for loads the drivers
 
         try {
-            Connection connection = DriverManager.getConnection(url,username,password);
-            Statement statement = connection.createStatement();
-            String query = "select * from students";
+            Connection connection = DriverManager.getConnection(url,username,password);//link with connection
+            Statement statement = connection.createStatement();//used to execute query
+            String query = "select * from students";//created query
             ResultSet resultSet = statement.executeQuery(query);
-            while (resultSet.next()){
+            while (resultSet.next()){ //till the database has next rows
                 int id = resultSet.getInt("id");
                 String name  = resultSet.getString("name");
                 int age = resultSet.getInt("age");
